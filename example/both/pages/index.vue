4a3e3f24c34bf0111f6dffd4297b1b386015661a
Ahad Birang <farnabaz@gmail.com>
2021-02-03 11:40:07 +0330

    next (#73)
    * Use PascalCase for component registration
    
    This allows both `<Recaptcha>` and `<recaptcha>` to be used in templates.
    
    Components that are registered in PascalCase can be used in templates with either PascalCase or kebab-case, whereas components registered in kebab-case can only be referenced in kebab-case. Some projects prefer to use PascalCase for component tags, so it's better for this plugin to make both options available.
    
    See https://vuejs.org/v2/guide/components-registration.html#Name-Casing
    
    * update plugin.js for global use
    
    * Moved script element error callback to within promise so that the error can be caught further up the stack
    
    * Reset ready state after error
    
    * docs: remove david badge
    
    * docs: add usage (#74)
    
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    
    * feat(multi-widget): render and verify multiple v2 widgets (#75)
    
    * feat(language): enable google auto detection (#72)
    
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    
    * fix: describe error within execute() (#40)
    
    Co-authored-by: Abdelhak Akermi <abdelhak@akermi.me>
    
    * feat: runtime config (#70)
    
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    Co-authored-by: Nikolay Baskov <baskov@adv.ru>
    
    * fix: remove badge on destroy (#76)
    
    * Apply suggestions from code review
    
    Co-authored-by: Simon Garner <simon.garner@madscience.co.nz>
    Co-authored-by: mvrlin <mvrlin@pm.me>
    Co-authored-by: lat1992 <lat1992@users.noreply.github.com>
    Co-authored-by: Owen Andrews <owen@owenandre.ws>
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    Co-authored-by: Red Bayoub <40964509+redbayoub@users.noreply.github.com>
    Co-authored-by: Abdelhak Akermi <abdelhak.akermi@gmail.com>
    Co-authored-by: Abdelhak Akermi <abdelhak@akermi.me>
    Co-authored-by: bason8800 <41577602+bason8800@users.noreply.github.com>
    Co-authored-by: Nikolay Baskov <baskov@adv.ru>

--- example/both/pages/index.vue
+++ example/both/pages/index.vue
@@ -4,20 +4,25 @@
 
     <form @submit.prevent="onSubmit">
       <input
+        v-model="email"
         autocomplete="true"
         placeholder="Email"
         type="email"
-        v-model="email"
       >
-      
+
       <input
+        v-model="password"
         autocomplete="current-password"
         placeholder="Password"
         type="password"
-        v-model="password"
       >
-      
-      <button type="submit">Sign In</button>
+      <recaptcha
+        id="v2-normal"
+        site-key="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+      />
+      <button type="submit">
+        Sign In
+      </button>
     </form>
   </section>
 </template>
@@ -27,25 +32,31 @@ export default {
   data: () => ({
     email: 'test@example.com',
     password: '123',
+    widgetId: 0
   }),
 
   async mounted() {
-    try {
-      await this.$recaptcha.init()
-    } catch (e) {
-      console.log(e);
-    }
+    await this.$recaptcha.init()
+
+    this.widgetId = this.$recaptcha.render('v2-normal', {
+      sitekey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+    })
   },
 
   methods: {
     async onSubmit() {
       try {
+        const tokenV2 = await this.$recaptcha.getResponse(this.widgetId)
+        console.log('V2 ReCaptcha token:', tokenV2)
+
         const token = await this.$recaptcha.execute('login')
-        console.log('ReCaptcha token:', token)
+        console.log('V3 ReCaptcha token:', token)
+
+        this.$recaptcha.reset(this.widgetId)
       } catch (error) {
         console.log('Login error:', error)
       }
-    },
-  },
+    }
+  }
 }
 </script>
